84bae5cbe468488fe77951ac9d5f3e5c3245319f
Pinont <Pinozenth.th@gmail.com>
2023-04-03 15:45:27 +0700

    init

---
+++ src/main/java/me/armored/core/Armored.java
@@ -0,0 +1,19 @@
+package me.armored.core;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Armored extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        Bukkit.getLogger().info();
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
